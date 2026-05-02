df84a636e247d18b62bd491dcdf5011f4c44595f
PaleNutcrackers <55241000+PaleNutcrackers@users.noreply.github.com>
2026-05-03 00:43:16 +0800

    整合机工基础GCD判定
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- Linto/LintoPvP/MCH/GCD/钻头.cs
+++ Linto/LintoPvP/MCH/GCD/钻头.cs
@@ -2,65 +2,32 @@ using AEAssist;
 using AEAssist.CombatRoutine.Module;
 using AEAssist.Extension;
 using AEAssist.Helper;
+using Linto.LintoPvP.MCH;
 using Linto.LintoPvP.PVPApi;
 
 namespace Linto.LintoPvP.MCH.GCD;
 
-public class 钻头 : ISlotResolver
+public class 钻头 : MCHSlotResolverBase
 {
-    public SlotMode SlotMode { get; }
-    private const uint SkillId = 29405u;
-    private const uint RequiredAura = 3150u;
-    private const int SkillRange = 25;
-    private const uint 分析Buff = 3158u;
-    private const uint 分析技能 = 29414u;
+    protected override string QtKey => "钻头";
+    protected override uint SkillId => 29405u;
+    protected override int SkillRange => 25;
+    protected override int? MaxGcdCooldownMs => 200;
+    protected override uint? RequiredAura => 3150u;
 
-    public int Check()
+    protected override int CheckSpecific()
     {
-        if (!PVPHelper.CanActive())
+        if (PvPMCHSettings.Instance.钻头分析 && !Core.Me.HasAura(3158u))
         {
-            return -1;
-        }
-        if (PVPHelper.MCH.IsMarksmanPreAnim())
-        {
-            return -2;
-        }
-        if (!PvPMCHOverlay.MCHQt.GetQt("钻头"))
-        {
-            return -4;
-        }
-        if (!Core.Me.HasAura(RequiredAura))
-        {
-            return -2;
-        }
-        if (GCDHelper.GetGCDCooldown() > 200)
-        {
-            return -3;
-        }
-        if (PVPHelper.通用距离检查(SkillRange))
-        {
-            return -5;
-        }
-        if (PVPHelper.通用技能释放Check(SkillId, SkillRange) == null)
-        {
-            return -6;
-        }
-        if (!SkillId.GetSpell().IsReadyWithCanCast() || PVPHelper.MCH.GetChangedAction(SkillId) != SkillId)
-        {
-            return -2;
-        }
-        if (PvPMCHSettings.Instance.钻头分析)
-        {
-            if (!Core.Me.HasAura(分析Buff))
+            if (SpellHelper.GetSpell(29414u).Charges > 0.5)
             {
-                if (SpellHelper.GetSpell(分析技能).Charges > 0.5)
-                {
-                    return -23;
-                }
+                return -23;
             }
+        }
+        if (PVPHelper.MCH.GetChangedAction(SkillId) != SkillId)
+        {
+            return -2;
         }
         return 0;
     }
-
-    public void Build(Slot slot) => PVPHelper.通用技能释放(slot, SkillId, SkillRange);
 }
--- Linto/LintoPvP/MCH/GCD/霰弹枪.cs
+++ Linto/LintoPvP/MCH/GCD/霰弹枪.cs
@@ -1,43 +1,15 @@
 using AEAssist.CombatRoutine.Module;
 using AEAssist.Helper;
+using Linto.LintoPvP.MCH;
 using Linto.LintoPvP.PVPApi;
 
 namespace Linto.LintoPvP.MCH.GCD;
 
-public class 霰弹枪 : ISlotResolver
+public class 霰弹枪 : MCHSlotResolverBase
 {
-    public SlotMode SlotMode { get; }
-    private const uint SkillId = 29404u;
-    private const int SkillRange = 12;
-
-    public int Check()
-    {
-        if (!PvPMCHOverlay.MCHQt.GetQt("霰弹枪"))
-        {
-            return -9;
-        }
-        if (!PVPHelper.CanActive())
-        {
-            return -1;
-        }
-        if (!SkillId.GetSpell().IsReadyWithCanCast())
-        {
-            return -2;
-        }
-        if (GCDHelper.GetGCDCooldown() > 200)
-        {
-            return -3;
-        }
-        if (PVPHelper.通用距离检查(SkillRange))
-        {
-            return -5;
-        }
-        if (PVPHelper.通用技能释放Check(SkillId, SkillRange) == null)
-        {
-            return -6;
-        }
-        return 0;
-    }
-
-    public void Build(Slot slot) => PVPHelper.通用技能释放(slot, SkillId, SkillRange);
+    protected override string QtKey => "霰弹枪";
+    protected override uint SkillId => 29404u;
+    protected override int SkillRange => 12;
+    protected override bool BlockInMarksmanPreAnim => false;
+    protected override int? MaxGcdCooldownMs => 200;
 }
